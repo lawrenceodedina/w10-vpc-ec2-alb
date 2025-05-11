6547e8cf317f709bcc002bcfe31171797af82da6
lawrenceodedina <lawrenceodedina@gmail.com>
2025-05-10 20:41:08 -0500

    Changed bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "w7-odedina-terraform"
+    bucket         = "hdvbhfbahbvb" #replace with s3 bucket
     key            = "week10/terraform.tf.state"
     region         = "us-east-1"
     dynamodb_table = "locktable"
